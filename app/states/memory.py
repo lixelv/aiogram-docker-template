72dd969ce8d0a05b51de56d333c275d6264b3c31
lixel_ <133605174+lixelv@users.noreply.github.com>
2025-03-11 13:05:52 +0300

    Added redis, not tested yet

--- app/states/memory.py
+++ app/states/memory.py
@@ -1,5 +1,9 @@
 from aiogram.fsm.storage.redis import RedisStorage
+<<<<<<< HEAD
 from redis.asyncio.client import Redis
+=======
+from redis.client import Redis
+>>>>>>> 61d6c2f (Added redis, not tested yet)
 
 from core import REDIS_CONFIG
 
